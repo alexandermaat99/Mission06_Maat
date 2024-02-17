f349e6b59951aa10a9f677e91fedac637fee16df
Alexander Maat <amaat@byu.edu>
2024-02-16 21:50:10 -0700

    Add project files.

---
+++ Mission06_Maat/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission06_Maat.Models;
+using System.Diagnostics;
+
+namespace Mission06_Maat.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) // constructor 
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult getToKnow()
+        {
+            return View();
+        }
+
+        public IActionResult movieCollection()
+        {
+            return View();
+        }
+    }
+}
